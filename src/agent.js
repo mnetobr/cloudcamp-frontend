2f5465b53625bc3fdeb4890b65b2c2bbb35227c1
mnetobr <53312970+mnetobr@users.noreply.github.com>
2019-08-03 10:14:43 -0300

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://backend.cloudcamp.cf/api';
+const API_ROOT = 'https://backend.mnetobr.cf/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
